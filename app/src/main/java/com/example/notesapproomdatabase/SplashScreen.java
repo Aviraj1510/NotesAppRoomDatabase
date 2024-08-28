7d2ef57a4e5e3cfba7d6c9114ed93735ef1bfd70
aviraj <panchotiyaaviraj@gmail.com>
2024-08-28 12:26:14 +0530

    SplashScreen.java Change

---
+++ app/src/main/java/com/example/notesapproomdatabase/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.notesapproomdatabase;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        videoView = findViewById(R.id.splashEntry);
+
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.add2);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+        videoView.start();
+    }
+
+    private void startNextActivity() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
